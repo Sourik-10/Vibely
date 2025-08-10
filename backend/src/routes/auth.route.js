103c9ee2e0ea188c935a99508e0c1bea46f67c13
Sourik-10 <ghoshsourik.48@gmail.com>
2025-08-11 00:05:36 +0530

    login fixed

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -11,7 +11,7 @@ import { protectRoute } from "../middleware/auth.middleware.js";
 const router = express.Router();
 
 router.post("/signup", signup);
-router.post("/login", login);
+router.post("/api/login", login);
 router.post("/logout", logout);
 router.get("/me", protectRoute, me);
 
