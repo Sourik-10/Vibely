288f53cedf005e5579c81775bcf295de97d7cba6
Sourik-10 <ghoshsourik.48@gmail.com>
2025-08-12 00:53:21 +0530

    /fix

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -59,7 +59,7 @@ export async function signup(req, res) {
   maxAge: 7 * 24 * 60 * 60 * 1000,
   httpOnly: true,
   sameSite: "none", // ✅ allows cross-site
-  secure: process.env.NODE_ENV === "production" // ✅ required with sameSite: none
+  secure: true  
 });
 
 
@@ -95,7 +95,7 @@ export async function login(req, res) {
   maxAge: 7 * 24 * 60 * 60 * 1000,
   httpOnly: true,
   sameSite: "none", // ✅ allows cross-site
-  secure: process.env.NODE_ENV === "production" // ✅ required with sameSite: none
+  secure: true 
 });
 
 
