2217a1c265ab54a0e3594a464314163a58869724
Sourik-10 <ghoshsourik.48@gmail.com>
2025-08-12 01:03:02 +0530

    /fix

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -108,9 +108,14 @@ export async function login(req, res) {
 }
 
 export function logout(req, res) {
-  res.clearCookie("jwt");
+  res.clearCookie("jwt", {
+    httpOnly: true,
+    sameSite: "none",
+    secure: true
+  });
   res.status(200).json({ success: true, message: "Logout successful" });
 }
+
 
 export async function me(req, res) {
   try {
